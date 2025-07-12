c34cff45523318d8b6a4a2f8b8d0b3c0e19dc034
BouleJaune <amthierry81@live.fr>
2025-07-12 17:13:14 +0200

    update

--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -84,12 +84,6 @@ _G.packer_plugins = {
     path = "/home/xenio/.local/share/nvim/site/pack/packer/start/LuaSnip",
     url = "https://github.com/L3MON4D3/LuaSnip"
   },
-  ["auto-session"] = {
-    config = { "\27LJ\2\nä\1\0\0\4\0\6\0\t6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\4\0005\3\3\0=\3\5\2B\0\2\1K\0\1\0\20suppressed_dirs\1\0\1\20suppressed_dirs\0\1\5\0\0\a~/\15~/Projects\16~/Downloads\6/\nsetup\17auto-session\frequire\0" },
-    loaded = true,
-    path = "/home/xenio/.local/share/nvim/site/pack/packer/start/auto-session",
-    url = "https://github.com/rmagatti/auto-session"
-  },
   ["cmp-nvim-lsp"] = {
     loaded = true,
     path = "/home/xenio/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
@@ -224,10 +218,6 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
--- Config for: auto-session
-time([[Config for auto-session]], true)
-try_loadstring("\27LJ\2\nä\1\0\0\4\0\6\0\t6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\4\0005\3\3\0=\3\5\2B\0\2\1K\0\1\0\20suppressed_dirs\1\0\1\20suppressed_dirs\0\1\5\0\0\a~/\15~/Projects\16~/Downloads\6/\nsetup\17auto-session\frequire\0", "config", "auto-session")
-time([[Config for auto-session]], false)
 -- Config for: oil.nvim
 time([[Config for oil.nvim]], true)
 try_loadstring("\27LJ\2\n1\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\boil\frequire\0", "config", "oil.nvim")
--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #picom &
 imwheel &
-xrandr --output HDMI-0 --primary
-/usr/bin/dunst
-xrandr --output DP-2 --right-of HDMI-0 &
+#xrandr --output HDMI-0 --primary
+/usr/bin/dunst &
+#xrandr --output DP-2 --right-of HDMI-0 &
 xrandr --output DP-2 --rate 180 --mode 2560x1440 &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 sleep 120
